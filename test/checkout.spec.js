4e16544b0e6d8c9374463ccd35fb0c722d6a8141
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-17 17:49:57 -0400

    refactor Checkout class instantiation

--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -1,14 +1,17 @@
 const Checkout = require('../checkout.js');
-
 var expect = require('chai').expect;
 
+var checkout;
+
+beforeEach(function(){
+    checkout = new Checkout();
+});
+
 it('Add item price', function(){
-    var checkout = new Checkout();
     checkout.addItemPrice('a', 1);
 });
 
-it('Add item', function() {
-    var checkout = new Checkout();
+it('Add item', function(){
     checkout.addItemPrice('a', 1);
     checkout.addItem('a');
 })
