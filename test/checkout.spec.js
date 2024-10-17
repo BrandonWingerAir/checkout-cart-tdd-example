980246441ed4736ed5a85bf951b8e8bbdb406069
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-17 17:37:42 -0400

    add item price test passed

--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -5,3 +5,8 @@ var expect = require('chai').expect;
 it('Instantiate checkout', function(){
     var checkout = new Checkout();
 });
+
+it('Add item price', function(){
+    var checkout = new Checkout();
+    checkout.addItemPrice('a', 1);
+});
