8c2b4877e3f5030f4c4b107cd9fadb58fa36863c
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-17 18:04:02 -0400

    add multiple items in total test pass

--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -12,3 +12,11 @@ it('Calculate current total', function(){
     checkout.addItem('a');
     expect(checkout.calculateTotal()).to.equal(1);
 });
+
+it('Add multiple items in total', function(){
+    checkout.addItemPrice('a', 1);
+    checkout.addItemPrice('b', 2);
+    checkout.addItem('a');
+    checkout.addItem('b');
+    expect(checkout.calculateTotal()).to.equal(3);
+});
