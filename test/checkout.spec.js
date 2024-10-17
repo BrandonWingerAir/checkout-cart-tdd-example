1fe3280cd378ab678b8110e29fedd89d4dccc403
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-17 17:41:25 -0400

    refactor add item price test

--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -2,10 +2,6 @@ const Checkout = require('../checkout.js');
 
 var expect = require('chai').expect;
 
-it('Instantiate checkout', function(){
-    var checkout = new Checkout();
-});
-
 it('Add item price', function(){
     var checkout = new Checkout();
     checkout.addItemPrice('a', 1);
