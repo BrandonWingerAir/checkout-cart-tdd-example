3f0e5ad42efbdf74ae55c0bbe896f5739a242a2b
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-20 13:35:58 -0400

    refactor add item price function calls

--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -5,17 +5,16 @@ var checkout;
 
 beforeEach(function(){
     checkout = new Checkout();
+    checkout.addItemPrice('a', 1);
+    checkout.addItemPrice('b', 2);
 });
 
 it('Calculate current total', function(){
-    checkout.addItemPrice('a', 1);
     checkout.addItem('a');
     expect(checkout.calculateTotal()).to.equal(1);
 });
 
 it('Add multiple items in total', function(){
-    checkout.addItemPrice('a', 1);
-    checkout.addItemPrice('b', 2);
     checkout.addItem('a');
     checkout.addItem('b');
     expect(checkout.calculateTotal()).to.equal(3);
